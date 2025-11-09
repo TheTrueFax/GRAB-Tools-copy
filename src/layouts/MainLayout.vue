06f240081f01bcc44b981ae6bf6d07addd3d2034
Tom Lynch <twhlynch@gmail.com>
2025-11-09 22:05:35 +1100

    fix: correctly scope page specific styles

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -9,24 +9,24 @@ export default {
 </script>
 
 <template>
-	<NavMenu />
-	<router-view />
-	<footer class="footer">
-		<span>twhlynch</span><span>•</span><span>.index</span>
-	</footer>
-	<Toast />
+	<div class="main-layout">
+		<NavMenu />
+		<router-view />
+		<footer class="footer">
+			<span>twhlynch</span><span>•</span><span>.index</span>
+		</footer>
+	</div>
 </template>
 
 <style>
-/* layout */
-html {
+html:has(.main-layout) {
 	background-color: #5f8bc2;
 	background-image: linear-gradient(#5f8bc2, #cfe0ea, #5f8bc2);
 	background-size: cover;
 	background-repeat: no-repeat;
 	background-attachment: fixed;
 }
-body {
+body:has(.main-layout) {
 	width: 100vw;
 	height: 100vh;
 	overflow-x: hidden;
@@ -34,122 +34,128 @@ body {
 	font-family: var(--font-family-default);
 	color: var(--text-color-default);
 }
-main {
-	width: min(800px, 80%);
-	margin-inline: auto;
-	margin-top: 100px;
-}
-@media screen and (max-width: 617px) {
-	main {
-		width: min(800px, 90%);
-	}
-}
-@media screen and (max-width: 400px) {
-	main {
-		width: 95%;
-		margin-top: 50px;
-	}
-}
-section {
-	padding: var(--padding-main);
-	width: 100%;
-}
 
-/* general */
-main {
-	p a {
-		color: var(--text-color-link);
+.main-layout {
+	* {
+		font-family: var(--font-family-default);
+	}
+	main {
+		width: min(800px, 80%);
+		margin-inline: auto;
+		margin-top: 100px;
+	}
+	@media screen and (max-width: 617px) {
+		main {
+			width: min(800px, 90%);
+		}
+	}
+	@media screen and (max-width: 400px) {
+		main {
+			width: 95%;
+			margin-top: 50px;
+		}
+	}
+	section {
+		padding: var(--padding-main);
+		width: 100%;
+	}
+
+	/* general */
+	main {
+		p a {
+			color: var(--text-color-link);
+			text-decoration: none;
+		}
+		h2 {
+			font-size: var(--font-size-big);
+			font-weight: var(--font-weight-bold);
+			color: var(--text-color-alt);
+			padding-bottom: var(--padding-secondary);
+		}
+	}
+	svg {
+		height: 24px;
+		width: 24px;
+		color: var(--text-color-default);
+	}
+	.button {
 		text-decoration: none;
-	}
-	h2 {
-		font-size: var(--font-size-big);
+		color: var(--text-color-default);
+		font-size: var(--font-size-default);
 		font-weight: var(--font-weight-bold);
-		color: var(--text-color-alt);
-		padding-bottom: var(--padding-secondary);
+		padding: var(--padding-secondary);
+		border-radius: var(--border-radius);
+		border: solid 2px var(--border-color);
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		align-items: center;
+		gap: var(--padding-secondary);
+		background-color: #0000;
+		cursor: pointer;
+		transition: background-color 0.2s ease-in-out;
+		&:hover {
+			background-color: #5f8cc213;
+		}
 	}
-}
-svg {
-	height: 24px;
-	width: 24px;
-	color: var(--text-color-default);
-}
-.button {
-	text-decoration: none;
-	color: var(--text-color-default);
-	font-size: var(--font-size-default);
-	font-weight: var(--font-weight-bold);
-	padding: var(--padding-secondary);
-	border-radius: var(--border-radius);
-	border: solid 2px var(--border-color);
-	display: flex;
-	flex-direction: row;
-	justify-content: flex-start;
-	align-items: center;
-	gap: var(--padding-secondary);
-	background-color: #0000;
-	cursor: pointer;
-	transition: background-color 0.2s ease-in-out;
-	&:hover {
-		background-color: #5f8cc213;
+	.button-sml {
+		text-decoration: none;
+		color: var(--text-color-default);
+		font-size: calc(var(--font-size-default) * 0.8);
+		font-weight: var(--font-weight-bold);
+		padding-block: calc(var(--padding-secondary) / 4);
+		padding-inline: calc(var(--padding-secondary) / 2);
+		border-radius: var(--border-radius);
+		border: solid 1px #4683ce70;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		gap: var(--padding-secondary);
+		background-color: #5f8cc235;
+		width: fit-content;
+		cursor: pointer;
 	}
-}
-.button-sml {
-	text-decoration: none;
-	color: var(--text-color-default);
-	font-size: calc(var(--font-size-default) * 0.8);
-	font-weight: var(--font-weight-bold);
-	padding-block: calc(var(--padding-secondary) / 4);
-	padding-inline: calc(var(--padding-secondary) / 2);
-	border-radius: var(--border-radius);
-	border: solid 1px #4683ce70;
-	display: flex;
-	flex-direction: row;
-	justify-content: center;
-	align-items: center;
-	gap: var(--padding-secondary);
-	background-color: #5f8cc235;
-	width: fit-content;
-	cursor: pointer;
-}
-.button-super-sml {
-	text-decoration: none;
-	color: var(--text-color-default);
-	font-size: calc(var(--font-size-default) / 2);
-	font-weight: var(--font-weight-bold);
-	padding-block: calc(var(--padding-secondary) / 8);
-	padding-inline: calc(var(--padding-secondary) / 4);
-	border-radius: var(--border-radius);
-	border: solid 1px #4683ce70;
-	display: flex;
-	flex-direction: row;
-	justify-content: flex-start;
-	align-items: center;
-	gap: var(--padding-secondary);
-	background-color: #5f8cc235;
-	width: fit-content;
-	cursor: pointer;
-}
+	.button-super-sml {
+		text-decoration: none;
+		color: var(--text-color-default);
+		font-size: calc(var(--font-size-default) / 2);
+		font-weight: var(--font-weight-bold);
+		padding-block: calc(var(--padding-secondary) / 8);
+		padding-inline: calc(var(--padding-secondary) / 4);
+		border-radius: var(--border-radius);
+		border: solid 1px #4683ce70;
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		align-items: center;
+		gap: var(--padding-secondary);
+		background-color: #5f8cc235;
+		width: fit-content;
+		cursor: pointer;
+	}
 
-/* footer */
-.footer {
-	margin-inline: auto;
-	width: fit-content;
-	height: 100px;
-	display: grid;
-	place-items: center;
-	grid-template-columns: 1fr 0.1fr 1fr;
-	gap: var(--padding-secondary);
-}
-.footer span {
-	width: 100%;
-}
-.footer span:nth-child(1) {
-	text-align: right;
-}
-.footer span:nth-child(2) {
-	text-align: center;
-}
-.footer span:nth-child(3) {
-	text-align: left;
+	/* footer */
+	.footer {
+		margin-inline: auto;
+		width: fit-content;
+		height: 100px;
+		display: grid;
+		place-items: center;
+		grid-template-columns: 1fr 0.1fr 1fr;
+		gap: var(--padding-secondary);
+	}
+	.footer span {
+		width: 100%;
+	}
+	.footer span:nth-child(1) {
+		text-align: right;
+	}
+	.footer span:nth-child(2) {
+		text-align: center;
+	}
+	.footer span:nth-child(3) {
+		text-align: left;
+	}
 }
 </style>
--- src/layouts/PlainLayout.vue
+++ src/layouts/PlainLayout.vue
@@ -3,8 +3,27 @@ export default {};
 </script>
 
 <template>
-	<router-view />
-	<Toast />
+	<div class="plain-layout">
+		<router-view />
+	</div>
 </template>
 
-<style scoped></style>
+<style>
+body:has(.plain-layout) {
+	width: 100vw;
+	height: 100vh;
+	font-size: var(--font-size-default);
+	font-family: var(--font-family-default);
+	color: var(--text-color-default);
+}
+.plain-layout {
+	width: 100vw;
+	height: 100vh;
+
+	svg {
+		height: 24px;
+		width: 24px;
+		color: var(--text-color-default);
+	}
+}
+</style>
