dc976c8de03f02447201b94f0c7fc5f5aa775a92
Tom Lynch <twhlynch@gmail.com>
2025-11-13 00:28:39 +1100

    feat: transformation icons

---
+++ src/icons/RotateIcon.vue
@@ -0,0 +1,25 @@
+<script>
+import IconTemplate from './IconTemplate.vue';
+
+export default {
+	components: {
+		IconTemplate,
+	},
+};
+</script>
+
+<template>
+	<IconTemplate>
+		<svg
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path d="M19.95 11a8 8 0 1 0-.5 4m.5 5v-5h-5" />
+		</svg>
+	</IconTemplate>
+</template>
---
+++ src/icons/ScaleIcon.vue
@@ -0,0 +1,27 @@
+<script>
+import IconTemplate from './IconTemplate.vue';
+
+export default {
+	components: {
+		IconTemplate,
+	},
+};
+</script>
+
+<template>
+	<IconTemplate>
+		<svg
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2"
+		>
+			<path
+				d="M3 15.396V21h5.604m6.792-18H21v5.604M21 3l-7.2 7.2m-3.6 3.6L3 21"
+			/>
+		</svg>
+	</IconTemplate>
+</template>
