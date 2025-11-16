813b9a32f9ee0c875eaa46c5437fa14d7bbd20ec
Tom Lynch <twhlynch@gmail.com>
2025-11-17 00:33:00 +1100

    feat: add pause icon

---
+++ src/icons/PauseIcon.vue
@@ -0,0 +1,25 @@
+<script>
+import IconTemplate from './IconTemplate.vue';
+
+export default {
+	components: {
+		IconTemplate,
+	},
+};
+</script>
+
+<template>
+	<IconTemplate>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		>
+			<path d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+		</svg>
+	</IconTemplate>
+</template>
