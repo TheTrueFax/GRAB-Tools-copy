f046741210ceb847f12594d62d072552ce8a5d36
Tom Lynch <twhlynch@gmail.com>
2025-11-17 15:52:02 +1100

    feat: allow loading a level from the ?level param

--- src/pages/EditorPage.vue
+++ src/pages/EditorPage.vue
@@ -4,6 +4,9 @@ import { useUserStore } from '@/stores/user';
 import { useConfigStore } from '@/stores/config';
 
 import encoding from '@/assets/tools/encoding';
+import { user_info_request } from '@/requests/UserInfoRequest';
+import { download_level_request } from '@/requests/DownloadLevelRequest';
+import { level_details_request } from '@/requests/LevelDetailsRequest';
 
 import MenuPanel from '@/components/EditorPanels/MenuPanel.vue';
 import ViewportPanel from '@/components/EditorPanels/ViewportPanel.vue';
@@ -38,14 +41,64 @@ export default {
 		...mapState(useConfigStore, ['default_level']),
 	},
 	mounted() {
-		this.json = this.default_level
-			? JSON.parse(JSON.stringify(this.default_level))
-			: encoding.createLevel();
-		this.set_json(this.json);
 		this.$refs.side_panel.size((window.innerHeight / 4) * 3);
 		this.$refs.main_panel.size((window.innerWidth / 3) * 2);
+		this.open_starting_level();
 	},
 	methods: {
+		async can_download_level(level_id) {
+			if (!this.is_logged_in) {
+				window.toast('Login to download your levels', 'warning');
+				return false;
+			}
+
+			const user_id = level_id.split(':')[0];
+
+			const user_info = await user_info_request(user_id);
+			if (user_info === null) return false;
+
+			if (this.user_name !== user_info.user_name) {
+				window.toast(
+					'You can only download your own levels',
+					'warning',
+				);
+				return false;
+			}
+
+			return true;
+		},
+		async download_level(level_id) {
+			let [user_id, map_id, iteration] = level_id.split(':');
+
+			const details = await level_details_request(level_id);
+			if (!details) return null;
+
+			iteration = iteration || details.iteration;
+			const download_id = [user_id, map_id, iteration].join(':');
+
+			const level = await download_level_request(download_id);
+			return level;
+		},
+		async open_starting_level() {
+			const params = new URLSearchParams(window.location.search);
+			const level_id = params.get('level');
+
+			if (level_id && (await this.can_download_level(level_id))) {
+				const level = await this.download_level(level_id);
+				if (level) {
+					const blob = new Blob([level], {
+						type: 'application/octet-stream',
+					});
+					this.json = await encoding.decodeLevel(blob);
+				}
+			} else {
+				this.json = this.default_level
+					? JSON.parse(JSON.stringify(this.default_level))
+					: encoding.createLevel();
+			}
+
+			this.set_json(this.json);
+		},
 		set_json(json, skip = []) {
 			this.json = json;
 			if (!skip.includes('json_panel'))
