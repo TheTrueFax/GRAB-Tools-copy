bae53a88d427004c29bf0cfcae7b4839b90df15a
Tom Lynch <twhlynch@gmail.com>
2025-11-24 20:40:50 +1100

    feat: some icons for the stats page

---
+++ src/icons/ListIcon.vue
@@ -0,0 +1,27 @@
+<script>
+import IconTemplate from './IconTemplate.vue';
+
+export default {
+	components: {
+		IconTemplate,
+	},
+};
+</script>
+
+<template>
+	<IconTemplate>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		>
+			<path
+				d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0-3.75-3.75M17.25 21 21 17.25"
+			/>
+		</svg>
+	</IconTemplate>
+</template>
