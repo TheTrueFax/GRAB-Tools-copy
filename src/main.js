6a0e890d014227c6bce6ebcc2141e937378d0c6a
Tom Lynch <twhlynch@gmail.com>
2025-11-29 23:43:02 +1100

    feat: sentry error monitoring

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import * as Sentry from '@sentry/vue';
 
 import * as config from './config';
 import router from './router';
@@ -12,6 +13,20 @@ const app = createApp(App);
 app.config.globalProperties.$config = config;
 
 app.config.unwrapInjectedRef = true;
+
+Sentry.init({
+	app,
+	// dummy dsn
+	dsn: 'https://SENTRY_PUBLIC_KEY@SENTRY_INGEST_HOST.ingest.us.sentry.io/0123456789',
+	tunnel: config.SERVER_URL + 'sentry_proxy',
+	sendDefaultPii: true,
+	enableLogs: true,
+	release: config.VERSION,
+	enabled: !(
+		window.location.hostname === 'localhost' ||
+		window.location.hostname === '127.0.0.1'
+	),
+});
 
 app.use(router);
 
